7b6d5d72e4f2c33dfbd272ab47f4d4c67d771d4b
SwethaPodhila <venkannapodhila@gmail.com>
2025-12-10 09:42:28 +0530

    changes

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use("/social", socialRoutes);
 app.use("/social/instagram", instagramRoutes);
 
 // publish & metrics
-app.post('/publish/facebook', facebookController.publish);
+//app.post('/publish/facebook', facebookController.publish);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT} 🚀`);
