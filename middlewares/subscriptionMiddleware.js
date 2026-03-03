c5ee661357f11c1b13fa2d83112c911473cb532f
SwethaPodhila <venkannapodhila@gmail.com>
2026-03-03 16:02:46 +0530

    fixed

--- middlewares/subscriptionMiddleware.js
+++ middlewares/subscriptionMiddleware.js
@@ -1,16 +1,14 @@
-// middlewares/subscriptionMiddleware.js
-
-import User from "../models/users.js";
+import User from "../models/userId.js";
 
 export const subscriptionMiddleware = async (req, res, next) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.userId);
 
     if (!user) {
       return res.status(404).json({ success: false, msg: "User not found" });
     }
 
-    // 🔥 Expiry Check
+    // Expiry Check
     if (user.planExpires && new Date() > user.planExpires) {
       console.log("⏳ Plan expired → Downgrading");
 
@@ -21,10 +19,13 @@ export const subscriptionMiddleware = async (req, res, next) => {
       await user.save();
     }
 
-    req.subscriptionUser = user; // pass updated user
+    req.subscriptionUser = user;
     next();
   } catch (err) {
     console.error("Subscription middleware error:", err);
-    res.status(500).json({ success: false, msg: "Server error in subscription middleware" });
+    res.status(500).json({
+      success: false,
+      msg: "Server error in subscription middleware",
+    });
   }
 };
